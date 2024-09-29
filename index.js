69f3f852e622ad50c81db4282cb187a6aa28ef51
Roman <hilixhilix1@gmail.com>
2024-09-29 10:40:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+
+let age = 17; 
+console.log(age);
+
+
+let name = "Roman";
+console.log(name);
+
+
+let isStudent = true; 
+console.log(isStudent);
+
+let myString = "Ваш улюблений цитата";
+console.log(myString);
+
+
+let myNumber = 20;
+myNumber += 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+
+let userName = prompt("Введіть ваше ім'я:");
+alert("Привіт, " + userName + "!");
+
+
+if (confirm("Ви впевнені, що хочете продовжити?")) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+
+
+if (confirm("Ця дія небезпечна! Ви впевнені, що хочете продовжити?")) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
